08ba807dd33979ef1e2ffe1349089bc2c7f00bc1
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-08 16:14:59 +0100

    Update Ejercicio13.java

--- 02_EjerciciosT2/Ejercicio13.java
+++ 02_EjerciciosT2/Ejercicio13.java
@@ -21,7 +21,7 @@ public class Ejercicio13 {
         System.out.println("Escribe tu nombre:");
         String nombre = new Scanner(System.in).nextLine();        
         System.out.println("Tu nombre en mayúsculas: "+nombre.toUpperCase());
-        System.out.println("Tu nombre en minúsculas: "+nombre.toLowerCase(Locale.ITALY));
+        System.out.println("Tu nombre en minúsculas: "+nombre.toLowerCase());
         System.out.println("Total de caracteres de tu nombre: "+nombre.length());
     }
     
