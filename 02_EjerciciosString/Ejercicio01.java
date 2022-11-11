f520815619c0c98c10a82e91543e268c6cdf319e
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-11 17:48:46 +0100

    Update Ejercicio01.java

--- 02_EjerciciosString/Ejercicio01.java
+++ 02_EjerciciosString/Ejercicio01.java
@@ -12,14 +12,19 @@ public class Ejercicio1StringsPython {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Consiste en eliminar los espacios en blanco iniciales en una cadena. Por ejemplo, la cadena ' Python rocks!' quedaría 'Python rocks!'"+"\n");
+        System.out.println("Consiste en eliminar los espacios en blanco iniciales en una cadena. Por ejemplo, la cadena ' Python rocks!' quedaría 'Python rocks!'"+"\n");        
         
-        System.out.println("Escriba texto:");
-        String cadena = new Scanner(System.in).nextLine();
+        String cadena = "       hola        "; 
         
-        System.out.println("\n"+"Cadena normal: "+cadena);
-        System.out.println("Cadena trim: "+cadena.trim()); 
-        System.out.println("Número de caracteres: "+cadena.trim().length());
+        System.out.println("\nCadena normal: "+cadena);
+        System.out.println("Cadena length inicial: "+cadena.length());
+                
+        String cadena2 = cadena.substring(0, cadena.lastIndexOf(" "));
+        String cadena2_1 = cadena2;
+        
+        
+        System.out.println("\nCadena sin espacios adelante:"+cadena2_1.trim());
+        System.out.println("Cadena length después: "+cadena2_1.length());
     }
     
 }
