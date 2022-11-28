59f62f7358b799314f211eeae3723aacb8ce03e3
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-29 00:49:54 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio37.java
@@ -0,0 +1,29 @@
+
+package ejercicio37;
+import java.util.Scanner;
+import java.lang.*;
+
+/* @author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
+public class Ejercicio37 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 37 : Consulta el pdf de la “Librería Estándar de Java” y busca la clase Integer, que está\n" +
+"en la hoja de “clases envoltorio”. Haz un programa que pregunte al usuario por teclado un\n" +
+"número. El programa mostrará ese número escrito en binario y hexadecimal.\n");
+        
+        System.out.println("Escriba un número: ");
+        int n = new Scanner(System.in).nextInt();   
+        
+        String bi = Integer.toBinaryString(n);
+        System.out.println("\nNúmero a binario: "+bi);
+        
+        String hex = Integer.toHexString(n);
+        System.out.println("Número a hexadecimal: "+hex);
+    }
+
+}
