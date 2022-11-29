92abadf3091b744b9dc74c5f6ec2ccb9804042bc
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-29 18:06:50 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio47.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio47;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author usuario-mañana
+ */
+public class Ejercicio47 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 47 : Consulta el pdf de la “Librería Estándar de Java” y busca la clase Desktop. Haz un\n" +
+"programa que pregunte al usuario la ruta de un archivo. El programa abrirá dicho archivo\n" +
+"usando para ello el programa que esté configurado en el sistema operativo.\n");
+        
+        System.out.println("Escriba una ruta:");
+        String ruta = new Scanner(System.in).nextLine();
+        File f = new File(ruta);
+        
+        try{
+            Desktop d = Desktop.getDesktop();
+            d.open(f);
+        }catch(Exception ex){
+            System.out.println("Error: "+ex.getMessage());
+        }
+        
+        
+        
+        
+    }
+    
+}
