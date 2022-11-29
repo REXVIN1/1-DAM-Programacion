e2f6eb4448e00bd6411b0142dc862474d7e0dc23
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-29 16:41:42 +0100

    Update Ejercicio38.java

--- 02_EjerciciosT2/Ejercicio38.java
+++ 02_EjerciciosT2/Ejercicio38.java
@@ -12,8 +12,10 @@ public class Ejercicio38 {
     public static void main(String[] args) {
         System.out.println("Ejercicio 38 : Haz un programa que calcule las dos soluciones de la ecuación x^2 − 5x + 6\n");
                 
-        double r1 = (-5+Math.sqrt(Math.pow(5, 2)-4*1*6))/2*1;
-        double r2 = (-5-Math.sqrt(Math.pow(5, 2)-4*1*6))/2*1;
+        int a=1,b=5
+        
+        double r1 = (-5+Math.sqrt(Math.pow(-5, 2)-4*1*6))/2*1;
+        double r2 = (-5-Math.sqrt(Math.pow(-5, 2)-4*1*6))/2*1;
         
         System.out.println("Resultado:");
         System.out.println("x = "+r1+" y x = "+r2);
