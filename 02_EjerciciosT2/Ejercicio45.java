b041806c0b6a913194e1fcb607b3746d55cb9cb4
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-11-29 17:47:23 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio45.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio45;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author usuario-mañana
+ */
+public class Ejercicio45 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 45 : Consulta el pdf de la librería “java time” y busca las clases LocalDate y\n" +
+"DayOfWeek. Haz un programa que muestre por pantalla el nombre del día de la semana que\n" +
+"será el 28 de febrero de 2100.\n");
+        
+        LocalDate hoy = LocalDate.of(2100, Month.FEBRUARY, 28);
+        
+        System.out.println(""+hoy.format(DateTimeFormatter.ofPattern("dd MMMM yyy")));
+    }
+    
+}
