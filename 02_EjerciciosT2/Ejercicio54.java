c7ce4f21caa9e468b0c741d359437aa40f17db7e
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-02 19:10:27 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio54.java
@@ -0,0 +1,33 @@
+
+package ejercicio54;
+import java.util.*;
+
+/*@author usuario-mañana*/
+
+public class Ejercicio54 {
+
+    /*@param args the command line arguments*/
+    
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 54 : Haz un programa que haga esto:\n");
+        
+        System.out.println("a) Crea una lista de 10 palabras (las que tú quieras)");
+        
+        ArrayList lista = new ArrayList();
+        
+        lista.add("dias");
+        lista.add("de");
+        lista.add("semana");
+        lista.add("lunes");
+        lista.add("martes");
+        lista.add("jueves");
+        lista.add("viernes");
+        lista.add("sabado");
+        lista.add("domingo");
+        
+        Collections.sort(lista);
+        
+        System.out.println(lista.toString());
+    }
+    
+}
