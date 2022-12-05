8cb8a5e4874f194713d65e1a3c27cab1889556d6
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-05 10:30:27 +0100

    Update Ejercicio44.java

--- 02_EjerciciosT2/Ejercicio44.java
+++ 02_EjerciciosT2/Ejercicio44.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio44;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
-/**
- *
- * @author usuario-mañana
- */
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
 public class Ejercicio44 {
 
-    /**
-     * @param args the command line arguments
-     */
+    /*@param args the command line arguments*/
+    
     public static void main(String[] args) {
         System.out.println("Ejercicio 44 : Consulta el pdf de la librería “java time” y busca las clases LocalDate y\n" +
 "DateTimeFormatter. Haz un programa que muestre por pantalla la fecha actual en formato\n" +
@@ -22,15 +16,11 @@ public class Ejercicio44 {
         
         LocalDate hoy = LocalDate.now();
         
-        String f1;
-        
         System.out.println("Fecha 1:");
-        f1 = hoy.format(DateTimeFormatter.ofPattern("dd/MM/yyy"));
-        System.out.println(f1);
+        System.out.println(hoy.format(DateTimeFormatter.ofPattern("dd/MM/yyy"));
         
         System.out.println("\nFecha 2:");
-        f1 = hoy.format(DateTimeFormatter.ofPattern("dd-MM-yyy"));
-        System.out.println(f1);       
+        System.out.println(hoy.format(DateTimeFormatter.ofPattern("dd-MM-yyy")));       
     }
     
 }
