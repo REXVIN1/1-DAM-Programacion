65cbfea0a8f909685cbb7840ff0e04237e5611a9
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-06 16:35:31 +0100

    Update Ejercicio12.java

--- 02_EjerciciosT2/Ejercicio12.java
+++ 02_EjerciciosT2/Ejercicio12.java
@@ -17,84 +17,30 @@ public class Ejercicio12 {
 "Estudiantes y el CB Granada. Registra las siguientes canastas y muestra el nombre de los\n" +
 "equipos, los puntos que tiene cada uno, el nombre del equipo que va ganando y el que va\n" +
 "perdiendo."+"\n");
-        
-        System.out.println("\u001B[32m"+"|------------------------------------------------|");
-        System.out.println("\u001B[32m"+"|"+"\u001B[30m"+"Partido de Baloncesto: CB Granada vs Estudiantes"+"\u001B[32m"+"|");
-        System.out.println("\u001B[32m"+"|------------------------------------------------|");
-        
+         
         MarcadorBaloncesto marcador = new MarcadorBaloncesto("CB Granada","Estudiantes");
         
-        System.out.println("Equipo 1: "+marcador.getNombreLocal());
-        System.out.println("Equipo 2: "+marcador.getNombreVisitante()+"\n");                
+        int puntos[] = {2,3,2,2,3,1,2};
+        char equipos[] = {'V','L','V','L','L','V','L'};
+        int p = 0,e = 0;
         
-        int puntos_local = marcador.getPuntosLocal();
-        int puntos_visitante = marcador.getPuntosVisitante();
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta0 = marcador.anotarCanasta('V',2 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta1 = marcador.anotarCanasta('L',3 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta3 = marcador.anotarCanasta('V',2 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta4 = marcador.anotarCanasta('L',2 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta5 = marcador.anotarCanasta('L',3 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta6 = marcador.anotarCanasta('V',1 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta7 = marcador.anotarCanasta('V',1 );
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
-        System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor()+"\n");
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
-        
-        boolean canasta8 = marcador.anotarCanasta('L',2 );      
-        
-        System.out.println("Marcador: "+marcador+"\n");        
-        System.out.println("Ganador: "+marcador.getNombreEquipoGanador());
-        System.out.println("Perdedor: "+marcador.getNombreEquipoPerdedor());
-        
-        System.out.println("\u001B[32m"+"----------------------------------------------------------");
+        System.out.println("--------EMPIEZA EL PARTIDO---------");
+        System.out.println(marcador);
+        System.out.println("-----------------------------------");
+        while(p<puntos.length){
+            System.out.println("\n-----------------------------------");
+            marcador.anotarCanasta(equipos[e], puntos[p]);
+            System.out.println(marcador);
+            System.out.println("Va ganando: "+marcador.getNombreEquipoGanador());
+            System.out.println("Va perdiendo: "+marcador.getNombreEquipoPerdedor());
+            p++;
+            e++;
+            System.out.println("-----------------------------------");
+        }
+        System.out.println("\n-----------RESULTADO---------------");
+        System.out.println("Equipo ganador: "+marcador.getNombreEquipoGanador());
+        System.out.println("Equipo ganador: "+marcador.getNombreEquipoPerdedor());
+        System.out.println("-----------------------------------");
     }
     
 }
