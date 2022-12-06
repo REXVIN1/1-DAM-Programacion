cd9d22ca8b719e6504b2367878d3e8f5c300d639
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-06 22:13:32 +0100

    Update Ejercicio29.java

--- 02_EjerciciosT2/Ejercicio29.java
+++ 02_EjerciciosT2/Ejercicio29.java
@@ -1,10 +1,8 @@
 /*
  */
 package ejercicio29;
-import java.util.*;
-import bpc.daw.objetos.*;
-import java.net.*;
-import java.io.*;
+import java.util.Scanner;
+import bpc.daw.objetos.Caja;
 /**
  *
  * @author Kevin Gómez <kgomval118@g.educaand.es>
@@ -21,31 +19,32 @@ public class Ejercicio29 {
         //ERROR: NumberFormatException
         System.out.println("a) Por teclado se pide que el usuario escriba un número y el usuario escribe un texto");
         try{
-            int numero = new Scanner(System.in).nextInt();  
-        }catch(Exception e1){
-            System.out.println("Error: "+e1.getMessage());
-        }   
+            System.out.println("Introduzca un numero:");
+            int n = new Scanner(System.in).nextInt();
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
         
         //ERROR: ArrayIndexOutOfBoundsException
         System.out.println("\nb) Un programa necesita recuperar dos argumentos de la línea de comandos y no se pasa" +
-"ninguno");   
+"ninguno"); 
         try{
             System.out.println("Argumento 1: " + args[0]);
             System.out.println("Argumento 2: " + args[1]);  
-        }catch(Exception e2){
-            System.out.println("Error: "+e2.getMessage());
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
         }
-                                           
+        
         //ERROR: ArrayIndexOutOfBoundsException
-        System.out.println("c) Creamos una caja vacía. Recuperamos el mensaje que tiene y mostramos por pantalla\n" +
+        System.out.println("\nc) Creamos una caja vacía. Recuperamos el mensaje que tiene y mostramos por pantalla\n" +
 "su longitud.");
         try{
-            Caja c = new Caja("");
+            Caja c = null;
             c.abrirCaja();
-            String mensaje = c.getMensaje();
-            System.out.println("Mensaje: "+mensaje);
-        }catch(Exception e3){
-            System.out.println("Error: "+e3.getMessage());
+            System.out.println("Mensaje: "+c.getMensaje());
+            System.out.println("Mensaje: "+c.getMensaje().length());
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
         }
         
     }
