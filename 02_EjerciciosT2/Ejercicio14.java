e2edc0cf2d84676556ad0ffcc00c019ac977319b
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-06 17:05:38 +0100

    Update Ejercicio14.java

--- 02_EjerciciosT2/Ejercicio14.java
+++ 02_EjerciciosT2/Ejercicio14.java
@@ -1,8 +1,7 @@
 /*
  */
 package ejercicio14;
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
 /**
  *
  * @author Kevin Gómez Valderas<kgomval118@g.educaand.es>
@@ -15,14 +14,25 @@ public class Ejercicio14 {
     public static void main(String[] args) {
         System.out.println("Ejercicio 14 : Consulta la documentación de la clase String para hacer un programa que\n" +
 "pregunte una frase al usuario y se muestre la letra que hay justo en la mitad de la frase."+"\n");
-        
-        
-        System.out.println("Escriba una frase:");
-        String frase = new Scanner(System.in).nextLine();        
-        
-        char letra = frase.charAt(frase.length()/2);
-        
-        System.out.println("\n "+"Letra central: "+letra);
+   
+        boolean b = true;
+        while(b==true){
+            System.out.println("\n----------------------------------------------------------");
+            System.out.println("Introduzca una frase:");
+            String frase = new Scanner(System.in).nextLine();
+            int longitud = frase.length();
+            int mitad = longitud/2;
+            if(longitud%2 == 0){
+                System.out.println("\nAl ser par no consta de una letra en mitad de la frase.");
+                int letra1 = mitad;
+                int letra2 = mitad+1;
+                System.out.println("En todo caso serian dos: '"+frase.charAt(letra1)+"' y '"+frase.charAt(letra2)+"'."); 
+                System.out.println("----------------------------------------------------------");
+            }else{
+                System.out.println("\nLa letra del medio es: "+frase.charAt(mitad++));
+                System.out.println("----------------------------------------------------------");
+            }
+        } 
     }
     
 }
