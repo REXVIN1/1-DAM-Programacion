d81dced42bf5ca6bbd1e7711cc481fa9da4206e1
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2022-12-07 14:53:22 +0100

    Update Ejercicio37.java

--- 02_EjerciciosT2/Ejercicio37.java
+++ 02_EjerciciosT2/Ejercicio37.java
@@ -1,7 +1,6 @@
 
 package ejercicio37;
 import java.util.Scanner;
-import java.lang.*;
 
 /* @author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
 
@@ -16,14 +15,10 @@ public class Ejercicio37 {
 "en la hoja de “clases envoltorio”. Haz un programa que pregunte al usuario por teclado un\n" +
 "número. El programa mostrará ese número escrito en binario y hexadecimal.\n");
         
-        System.out.println("Escriba un número: ");
-        int n = new Scanner(System.in).nextInt();   
-        
-        String bi = Integer.toBinaryString(n);
-        System.out.println("\nNúmero a binario: "+bi);
-        
-        String hex = Integer.toHexString(n);
-        System.out.println("Número a hexadecimal: "+hex);
+        System.out.println("Escriba un número:");
+        int n = new Scanner(System.in).nextInt();
+        System.out.println("\nNúmero en binario: "+Integer.toBinaryString(n));
+        System.out.println("Número en hexadecimal: "+Integer.toHexString(n));
     }
 
 }
