93fcd47a17966985603e113d9e5f9d23f37366a3
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-10 16:03:42 +0100

    Add files via upload

--- 03_EjerciciosT3/EjemploNombre.java
+++ 03_EjerciciosT3/EjemploNombre.java
@@ -1,5 +1,6 @@
 
 package nombre;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*@author usuario-mañana*/
@@ -10,15 +11,20 @@ public class EjemploNombre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Introduzca 5 nombres: ");
-        String nombre = new Scanner(System.in).nextLine();
+        ArrayList al = new ArrayList();        
         int n = 1;
-        System.out.println("Nombre "+n+": ");        
+        System.out.println("Introduzca 5 nombres: ");        
+        String nombre = new Scanner(System.in).nextLine();
+        al.add(nombre);
+        System.out.println("Nombre "+n+": "+nombre);    
+        
         while(n!=5){
-            nombre = new Scanner(System.in).nextLine();            
+            nombre = new Scanner(System.in).nextLine();
+            al.add(nombre);
             n++;
             System.out.println("Nombre: "+n+": "+nombre);
         }
+        System.out.println("\nNombres: "+al);
     }
     
 }
