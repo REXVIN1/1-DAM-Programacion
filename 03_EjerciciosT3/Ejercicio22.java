7084375bd5850ce010741e2a4be7794391304dc0
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-17 14:40:29 +0100

    Update Ejercicio22.java

--- 03_EjerciciosT3/Ejercicio22.java
+++ 03_EjerciciosT3/Ejercicio22.java
@@ -9,7 +9,8 @@ public class Ejercicio22 {
         int pepe[]={9,5,4,3}, ana[]={4,8,6,5}, juan[]={2,5,10,2}, 
                 guillermo[]={0,0,5,0}, roberta[]={10,10,10,9},
                 total_pepe=0, total_ana=0, total_juan=0, total_guillermo=0,
-                total_roberta=0;        
+                total_roberta=0;
+        
         for(int i=0;i<3;i++){
             total_pepe = total_pepe + pepe[i];
             total_ana = total_ana + ana[i];
