62b59752e5a92fcff46262e36e9c63cac5c33232
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-19 15:18:26 +0100

    Update Ejercicio42.java

--- 03_EjerciciosT3/Ejercicio42.java
+++ 03_EjerciciosT3/Ejercicio42.java
@@ -7,24 +7,26 @@ import java.util.Scanner;
 public class Ejercicio42 {
 
     public static void main(String[] args) {
-        int f, c, e, x=0;
+        int f, c,e,x = 0;
         String fila = "";
-        
-        System.out.println("Escriba numero de filas:");
-        f = new Scanner(System.in).nextInt();
-        System.out.println("Escriba numero de columnas:");
-        c = new Scanner(System.in).nextInt();
-        
-        for(int i=0;i<f;i++){  
-            e=0+i;
-            while(x<c){
-                fila = fila+" "+e;
-                e++;
-                x++;
-            }
-            System.out.println(fila);
-            fila = "";
-            x=0;                        
-        }                  
+        boolean b = true;
+        while(b){
+            System.out.println("\nEscriba numero de filas:");
+            f = new Scanner(System.in).nextInt();
+            System.out.println("Escriba numero de columnas:");
+            c = new Scanner(System.in).nextInt();
+
+            for(int i=0;i<f;i++){  
+                e=0+i;
+                while(x<c){
+                    fila = fila+" "+e;
+                    e++;
+                    x++;
+                }
+                System.out.println(fila);
+                fila = "";
+                x=0;                        
+            }                          
+        }
     }
 }
