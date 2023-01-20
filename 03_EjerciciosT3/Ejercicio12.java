86273ebd336886d9a4657e035a6d9d43c2e94e86
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-20 16:29:35 +0100

    Update Ejercicio12.java

--- 03_EjerciciosT3/Ejercicio12.java
+++ 03_EjerciciosT3/Ejercicio12.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Ejercicio12 {
 
     public static void main(String[] args) {
-        int lista[] = {0,1,2,3,4,5,6,7,8,9,10},n, i = 0, longitud = lista.length, total = 0;
+        int lista[] = {0,1,2,3,4,5,6,7,8,9,10}, n, longitud=lista.length, total=0;
         boolean b = true;
         while(b){
             System.out.println("Introduzca -1 para ver las notas: ");
@@ -15,9 +15,8 @@ public class Ejercicio12 {
             if(n==-1){
                 System.out.println("Nota máximo: "+Math.max(lista[0], lista[10])+""
                         + "\nNota mínimo: "+Math.min(lista[0], lista[10]));
-                while(i<longitud){
+                for(i=0;i<longitud;i++){
                     total = total+lista[i];
-                    i++;
                 }
                 System.out.println("Nota media: "+(total/longitud));
                 b=false;
