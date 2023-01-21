3e8e6aa10d7837aede437c9e5b5f339a765848f7
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-21 20:29:40 +0100

    Update Ejercicio02.java

--- 03_EjerciciosT3/REPASO/Ejercicio02.java
+++ 03_EjerciciosT3/REPASO/Ejercicio02.java
@@ -8,7 +8,7 @@ public class Ejercicio02 {
     public static void main(String[] args) {
         
         for(int i=0;i<8;i++){
-            System.out.println("********");
+            System.out.println("* * * * * * * *");
         }
     } 
 }
