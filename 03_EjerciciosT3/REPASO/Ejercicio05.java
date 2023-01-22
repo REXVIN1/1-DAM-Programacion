a48d3091b82f97eef62781f53fff608f203be788
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-22 15:00:03 +0100

    Update Ejercicio05.java

--- 03_EjerciciosT3/REPASO/Ejercicio05.java
+++ 03_EjerciciosT3/REPASO/Ejercicio05.java
@@ -7,15 +7,14 @@ package ejercicio05;
 public class Ejercicio05 {
 
     public static void main(String[] args) {
-        
         int x=0, y=1;
-        String numeros = "";
+        String numeros =  "";
         
-        while(y<=50){
-            numeros = numeros +x+" "+y+" ";            
-            x=x+y;
-            y=x+y;
+        for(int i=0;i<50;i++){
+            numeros = numeros +x+","+y+",";            
+            x = x+y;
+            y = x+y;
         }
-        System.out.println("Numeros Fibonacci: "+numeros);
+        System.out.println(numeros);
     }
 }
