5d3bee49d84a3972613f4c236041682b1d5e75a2
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-01-23 11:51:08 +0100

    Update Ejercicio10.java

--- 03_EjerciciosT3/REPASO/Ejercicio10.java
+++ 03_EjerciciosT3/REPASO/Ejercicio10.java
@@ -9,12 +9,11 @@ import java.util.Scanner;
 public class Ejercicio10 {
 
     public static void main(String[] args) {
-        
         List<String> normal = new ArrayList<>();
         List<String> leet = new ArrayList<>();
         String texto, letra;
         int x=0, y=1;
-        boolean b;
+        boolean b=true;
         
         String[] abecedario = {"A","B","C","D","E","F","G","H","I","J","K","L",
             "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
@@ -29,19 +28,23 @@ public class Ejercicio10 {
         System.out.println(normal);
         System.out.println(leet);
         
-        System.out.println("\nEscriba un texto:");
-        texto = new Scanner(System.in).nextLine();
-        
-        for(int i=0;i<texto.length();i++){
-            letra = texto.substring(x, y);
-            for(int e=0;e<normal.size();e++){
-                if(letra.equalsIgnoreCase(normal.get(e))){
-                    texto = texto.replaceAll(letra, leet.get(e)); 
-                }                                                                 
-            }                       
-            x++;
-            y++;
-        } 
-        System.out.println(texto);        
+        while(b){
+            System.out.println("\nEscriba un texto:");
+            texto = new Scanner(System.in).nextLine();
+
+            for(int i=0;i<texto.length();i++){
+                letra = texto.substring(x, y);
+                for(int e=0;e<normal.size();e++){
+                    if(letra.equalsIgnoreCase(normal.get(e))){
+                        texto = texto.replaceAll(letra, leet.get(e)); 
+                    }                                                                 
+                }                       
+                x++;
+                y++;
+            } 
+            System.out.println(texto);
+            x=0;
+            y=1;
+        }       
     }
 }
