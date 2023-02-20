bc5c8b228492de164d2b83900e858d4c1fde95b6
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-02-20 20:32:56 +0100

    Add files via upload

---
+++ 04_EjerciciosT4/Monedero.java
@@ -0,0 +1,37 @@
+
+package daw.persona;
+
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
+public class Monedero implements Adinerado{
+    //VALORES
+    private double dinero;
+    
+    //CONSTRUCTOR
+    public Monedero(){
+        this.dinero = 0;
+    }
+
+    @Override
+    public double getDineroTotal() {
+        return this.dinero;
+    }
+
+    @Override
+    public boolean añadirDinero(int cantidad) {
+        boolean b = cantidad<=1000;        
+        if(b){
+            this.dinero+=cantidad;            
+        }
+        return b;
+    }
+
+    @Override
+    public boolean retirarDinero(int cantidad) {
+        boolean b = cantidad<=this.dinero; 
+        if(b){
+            this.dinero-=cantidad;            
+        }
+        return b;
+    }
+}
