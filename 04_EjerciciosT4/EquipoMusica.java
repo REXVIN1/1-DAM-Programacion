8b92aee28a6bea6b98167b4545724d05a970a2ad
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-02-20 19:35:54 +0100

    Add files via upload

---
+++ 04_EjerciciosT4/EquipoMusica.java
@@ -0,0 +1,31 @@
+
+package daw.persona;
+
+import java.util.ArrayList;
+
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
+public class EquipoMusica {
+    //VALORES
+    private Altavoz[] altavoces;
+    
+    //CONSTRUCTOR
+    public EquipoMusica(int numeroAltavoces){
+        this.altavoces = new Altavoz[numeroAltavoces];
+        for(int i=0;i<numeroAltavoces;i++){
+            Altavoz a = new Altavoz();  
+            this.altavoces[i] = a;
+        }
+    }
+    
+    //GETTERS
+    public Altavoz getAltavoz(int posicion){
+        return this.altavoces[posicion];
+    }
+    
+    
+    //SETTERS
+    public void setVolumen(int numeroAltavoz, int volumen){
+        this.altavoces[numeroAltavoz].setVolumen(volumen); 
+    }
+}
