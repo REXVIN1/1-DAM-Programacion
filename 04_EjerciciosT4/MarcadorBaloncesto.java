d71ebeae70c3a6a3c2d3ae48686f194e361b29d8
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-02-20 10:17:20 +0100

    Update MarcadorBaloncesto.java

--- 04_EjerciciosT4/MarcadorBaloncesto.java
+++ 04_EjerciciosT4/MarcadorBaloncesto.java
@@ -1,18 +1,21 @@
-/*
- */
 package daw.persona;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 
 /*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
 
 public class MarcadorBaloncesto {
+    //VALORES
     private String nombreLocal;
     private String nombreVisitante;
     private int puntosLocal;
     private int puntosVisitante;
     LocalDate fecha;
     
+    //CONSTRUCTORES
     public MarcadorBaloncesto(String nL, String nV){
         nombreLocal = nL;
         nombreVisitante = nV;
@@ -36,19 +39,32 @@ public class MarcadorBaloncesto {
     }
     
     //SETTERS
-    public void añadirCanasta(char equipo,int puntos){
-        if(puntos>0&&puntos<=3){
-            if(equipo=='L'){
-                puntosLocal+=puntos;
-            }if(equipo=='V'){
-                puntosVisitante+=puntos;
+    public void añadirCanasta(char equipo,int puntos) throws IllegalArgumentException{
+        try{
+            if(puntos>0&&puntos<=3){
+                if(equipo=='L'){
+                    puntosLocal+=puntos;
+                }if(equipo=='V'){
+                    puntosVisitante+=puntos;
+                }            
             }            
-        }
+        }catch(IllegalArgumentException e){}
     }
     public void reset(){
         puntosLocal = 0;
         puntosVisitante = 0;        
     }
+    public void guardar(String ruta) throws IOException{
+        File f = new File(ruta);
+        f.createNewFile();
+        PrintWriter pw = new PrintWriter(f);
+        pw.println(nombreLocal);
+        pw.println(puntosLocal);
+        pw.println(nombreVisitante);
+        pw.println(puntosVisitante);
+        pw.close();    
+    }
+    
     
     //GETTERS
     public int getPuntosLocal(){
