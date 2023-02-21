f4c5e0231fbdd7e94699b13bd26b6eae2b7d5275
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-02-21 12:47:48 +0100

    Add files via upload

---
+++ 04_EjerciciosT4/Matricula.java
@@ -0,0 +1,30 @@
+
+package daw.persona;
+
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
+public class Matricula {
+    //VALORES
+    private static int siguienteNúmeroMatricula = 1;
+    private int numeroMatricula;
+    private String nombreAlumno;
+    private String nombreAsignatura;
+    
+    //CONSTRUCTORES
+    public Matricula(String nombreAlumno, String nombreAsignatura){
+        this.nombreAlumno = nombreAlumno;
+        this.nombreAsignatura = nombreAsignatura;
+        this.numeroMatricula = this.siguienteNúmeroMatricula++;
+    }
+    
+    //GETTERS
+    public String getNombre(){
+        return this.nombreAlumno;
+    }
+    public String getNombreAsignatura(){
+        return this.nombreAsignatura;
+    }
+    public int getNumeroMatricula(){
+        return this.numeroMatricula;
+    }
+}
