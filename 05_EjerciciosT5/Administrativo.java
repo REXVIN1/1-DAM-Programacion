679889cd28e9f56d6f8a42bfa0edd0bc7042cb30
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-03-08 16:20:52 +0100

    Add files via upload

---
+++ 05_EjerciciosT5/Administrativo.java
@@ -0,0 +1,29 @@
+
+package tema5;
+
+import java.time.LocalDateTime;
+
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
+
+public class Administrativo extends TrabajadorTecleante{
+    //CONSTRUCTOR
+    public Administrativo(String n, int s, int ndni, char ldni, int ppm){
+        super(n, s, ndni, ldni, ppm);
+    }
+    
+    //SETTER
+    public void escribirInforme(String t){
+        LocalDateTime fecha_hora = LocalDateTime.now();
+        String informe = fecha_hora.toString(); 
+    }
+    
+    //TO STRING
+    @Override
+    public String toString(){
+        String s = "ADMINISTRATIVO:\nNombre: "+Administrativo.super.getNombre()+
+                "\nSueldo: "+Administrativo.super.getSueldo()+
+                "\nDNI: "+Administrativo.super.getDNI()+
+                "\nPPM: "+Administrativo.super.getPulsacionesMinuto();
+        return s;
+    }
+}
