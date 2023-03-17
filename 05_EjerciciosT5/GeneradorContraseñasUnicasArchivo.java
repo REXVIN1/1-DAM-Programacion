f085d69fc604f953a6e5d5ae4a9f440076c88e12
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-03-17 15:09:21 +0100

    Add files via upload

--- 05_EjerciciosT5/GeneradorContraseñasUnicasArchivo.java
+++ 05_EjerciciosT5/GeneradorContraseñasUnicasArchivo.java
@@ -1,32 +1,38 @@
-
+/*
+ */
 package tema5;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
-/*@author usuario-tarde*/
+/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
 
-public class GeneradorContraseñasArchivo extends GeneradorContraseñas{
+public class GeneradorContraseñasUnicasArchivo extends GeneradorContraseñasUnicas{
     //VALORES
     private File archivo;
     private Random random;
+    private Set<String> contraseñasGeneradas;
     
     //CONSTRUCTOR
-    public GeneradorContraseñasArchivo(String ruta){
+    public GeneradorContraseñasUnicasArchivo(String ruta){
         this.archivo = new File(ruta);
         this.random = new Random();
+        this.contraseñasGeneradas = new HashSet<>();
     }
-    public GeneradorContraseñasArchivo(String ruta, Random r){
+    public GeneradorContraseñasUnicasArchivo(String ruta, Random r){
         this.archivo = new File(ruta);
         this.random = r;
+        this.contraseñasGeneradas = new HashSet<>();
     }
     
     //SETTERS
     @Override
     public String generarContraseña(int longitud){
-        String contraseña = GeneradorContraseñasArchivo.super.generarContraseña();
+        String contraseña = GeneradorContraseñasUnicasArchivo.super.generarContraseña();
         try {
             FileWriter fw = new FileWriter(this.archivo);
             if(this.archivo.exists()){
