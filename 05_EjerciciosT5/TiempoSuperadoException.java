70f724bd8ca702c0ddeb3efaa4f2bc5edcb6563f
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-03-20 10:08:42 +0100

    Update TiempoSuperadoException.java

--- 05_EjerciciosT5/TiempoSuperadoException.java
+++ 05_EjerciciosT5/TiempoSuperadoException.java
@@ -4,6 +4,11 @@ package tema5;
 /*@author usuario-tarde*/
 
 public class TiempoSuperadoException extends AdivinanzaException {
+    
+    //CONTRUCTOR POR DEFECTO
+    public TiempoSuperadoException(String mensaje){}
+     
+    //CONSTRUCTOR
     public TiempoSuperadoException(String mensaje){
         super(mensaje);
     }
