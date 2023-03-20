d9cf8206d25f8783e19a0ea48ce999a68933c5c1
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-03-20 10:07:36 +0100

    Update AdivinanzaException.java

--- 05_EjerciciosT5/AdivinanzaException.java
+++ 05_EjerciciosT5/AdivinanzaException.java
@@ -4,6 +4,11 @@ package tema5;
 /*@author usuario-tarde*/
 
 public class AdivinanzaException extends Exception {
+   
+   //CONSTRUCTOR POR DEFECTO
+    public AdivinanzaException(){}
+    
+    //CONSTRUCTOR
     public AdivinanzaException(String mensaje){
         super(mensaje);
     }
