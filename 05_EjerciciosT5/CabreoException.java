4e8d146dd965091b5ac3b30da016e13935e536bd
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-03-20 10:06:58 +0100

    Update CabreoException.java

--- 05_EjerciciosT5/CabreoException.java
+++ 05_EjerciciosT5/CabreoException.java
@@ -4,6 +4,9 @@ package tema5;
 /*@author usuario-tarde*/
 
 public class CabreoException extends Exception{
+    //CONSTRUCTOR POR DEFECTO
+    public CabreoException(){}
+    
     //CONSTRUCTOR
     public CabreoException(String m){
         super(m);
