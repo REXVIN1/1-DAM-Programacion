23baaba13b838dac734236af27836488b25638c8
Kevin Gómez <114946134+REXVIN1@users.noreply.github.com>
2023-04-24 21:25:22 +0200

    Delete 05_EjerciciosT5 directory

--- 05_EjerciciosT5/Nombrable.java
+++
@@ -1,9 +0,0 @@
-
-package tema5;
-
-/*@author usuario-tarde*/
-
-public interface Nombrable {
-    //GETTER
-    public String getNombre();
-}
--- 05_EjerciciosT5/Teclado.java
+++
@@ -1,9 +0,0 @@
-/*
- */
-package tema5;
-
-/*@author Kevin Gómez Valderas<kgomval118@g.educaand.es>*/
-
-public interface Teclado{
-    public void escribirTexto(String mensaje);
-}
